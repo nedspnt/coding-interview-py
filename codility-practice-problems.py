18c791688ea37dde61c44b9bf1c8de7700914067
nedspnt <nedspnt@gmail.com>
2022-07-18 02:16:08 +0700

    first codility practice problem

---
+++ codility-practice-problems.py
@@ -0,0 +1,15 @@
+# PROBLEM 1: maxmimum zero gap for binary
+
+def maximum_zero_gap(array):  
+    """ Given a a number, get the longest zeros (between ones) of its binary
+
+    Args:
+        array (list): a list-like object
+
+    Returns:
+        num_max_zeros (int): maximum number of zeros between ones
+    """
+
+    num_max_zeros = len(max(bin(array).strip('0').split('1')))
+
+    return num_max_zeros
